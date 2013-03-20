eee2acfef245ebaa1be1b9d618613469d5c9c7e9
Ben Witzen <10189459@u019917.fnwi.uva.nl>
2013-03-20 12:09:19 +0100

    backup

--- framework6/main.cpp
+++ framework6/main.cpp
@@ -11,12 +11,13 @@
  */
 
 #include <cstdio>
-
 #include <GL/gl.h>
 #include <GL/glut.h>
 #include <Box2D/Box2D.h>
-
 #include "levels.h"
+
+#define TIMESTEP    1.0 / 60.0
+#define PI_VAL      3.1415
 
 unsigned int reso_x = 800, reso_y = 600; // Window size in pixels
 const float world_x = 8.f, world_y = 6.f; // Level (world) size in meters
@@ -29,22 +30,97 @@ unsigned int num_levels;
 level_t *levels;
 
 
+b2Vec2 gravity(0.0f, -10.0f);
+b2World world(gravity);
+b2Body* body;
+b2Body* stupidpolygon;
+b2Body* groundBody;
+b2Body* circleBody;
+//b2CircleShape circle;
+b2PolygonShape polygon;
+//b2PolygonShape polygon;
+
+/*
+ * Function that can draw a circle. Hooray!
+ */
+
+void draw_circle(float x, float y, float r) {
+  // begin line segment
+  glBegin(GL_LINE_LOOP);
+  float rad;  
+  // draw circle
+  for (int i = 0; i < 360; i++) {
+    rad = i * PI_VAL / 180.0;
+    glVertex2f(cos(rad)*r + x, sin(rad)*r + y);
+  }
+  // end
+  glEnd();
+}
+
+
 /*
  * Load a given world, i.e. read the world from the `levels' data structure and
  * convert it into a Box2D world.
  */
 void load_world(unsigned int level)
 {
-    if (level >= num_levels)
-    {
-        // Note that level is unsigned but we still use %d so -1 is shown as
-        // such.
-        printf("Warning: level %d does not exist.\n", level);
-        return;
-    }
+  if (level >= num_levels) {
+    printf("Warning: level %d does not exist.\n", level);
+    return;
+  }
 
-    // Create a Box2D world and populate it with all bodies for this level
-    // (including the ball).
+  //
+  // THIS PIECE OF CODE CREATES THE GROUND
+  //
+
+  b2BodyDef groundBodyDef;
+  groundBodyDef.position.Set(0.0f, -10.0f);
+  groundBody = world.CreateBody(&groundBodyDef);
+  b2PolygonShape groundBox;
+  groundBox.SetAsBox(50.0f, 10.0f);
+  groundBody->CreateFixture(&groundBox, 0.0f);
+
+  //
+  // THIS PIECE OF CODE CREATES THE BALL
+  //
+
+  // create ball
+  b2BodyDef ball;
+  ball.type = b2_dynamicBody;
+  ball.position.Set(levels[1].start.x, levels[1].start.y);
+  body = world.CreateBody(&ball);
+  b2Vec2 position = body->GetPosition();
+  // create a shape
+  b2PolygonShape dynamicBox;
+  dynamicBox.SetAsBox(.075f, .075f);
+  // create a fixture
+  b2FixtureDef fixtureDef;
+  fixtureDef.shape = &dynamicBox;
+  fixtureDef.density = 1.0f;
+  fixtureDef.friction = .3f;
+  // apply fixture to body
+  body->CreateFixture(&fixtureDef);
+
+  //
+  // THIS PIECE OF CODE CREATES THE POLYGONS
+  // 
+
+  b2BodyDef whatever;
+  groundBodyDef.position.Set(0.0f, 0.0f);
+  b2Vec2 vertices[3];
+  vertices[0].Set(1.0f, 4.0f);
+  vertices[1].Set(1.0f, 1.0f);
+  vertices[2].Set(6.0f, 1.0f);
+  int32 count = 3;
+  polygon.Set(vertices, count);
+  stupidpolygon = world.CreateBody(&whatever);
+
+  fixtureDef.shape = &polygon;
+  fixtureDef.density = 1.0f;
+  stupidpolygon->CreateFixture(&fixtureDef);
+  stupidpolygon->ResetMassData();
+
+
 }
 
 
@@ -52,36 +128,48 @@ void load_world(unsigned int level)
  * Called when we should redraw the scene (i.e. every frame).
  * It will show the current framerate in the window title.
  */
-void draw(void)
-{
-    int time = glutGet(GLUT_ELAPSED_TIME);
-    int frametime = time - last_time;
-    frame_count++;
+void draw(void) {
+  int time = glutGet(GLUT_ELAPSED_TIME);
+  int frametime = time - last_time;
+  frame_count++;
 
-    // Clear the buffer
-    glColor3f(0, 0, 0);
-    glClear(GL_COLOR_BUFFER_BIT);
+  // Clear the buffer
+  glColor3f(0, 0, 0);
+  glClear(GL_COLOR_BUFFER_BIT);
 
+  // cause the world to step
+  world.Step(TIMESTEP, 6, 2);
+  b2Vec2 position = body->GetPosition();
+  float32 angle = body->GetAngle();
+  // print ball position to terminal
+  printf("%4.2f %4.2f %4.2f\n", position.x, position.y, angle);
 
-    //
-    // Do any logic and drawing here.
-    //
+  // prepare openGL for use
+  glLineWidth(3.0);
+  glColor3f(1.0, 0.0, 0.0);
+  // call our own draw function to make that circle visible!
+  draw_circle(position.x, position.y, .075f);
+  // draw that fooking polygon
+  //glBegin(GL_TRIANGLES);
+  //glVertex3f(1.0f, 4.0f, 0);
+  //glVertex3f(1.0f, 1.0f, 0);
+  //glVertex3f(6.0f, 1.0f, 0);
+  // end
+  //glEnd();  
 
+  // Show rendered frame
+  glutSwapBuffers();
 
-    // Show rendered frame
-    glutSwapBuffers();
-
-    // Display fps in window title.
-    if (frametime >= 1000)
-    {
-        char window_title[128];
-        snprintf(window_title, 128,
-                "Box2D: %f fps, level %d/%d",
-                frame_count / (frametime / 1000.f), -1, num_levels);
-        glutSetWindowTitle(window_title);
-        last_time = time;
-        frame_count = 0;
-    }
+  // Display fps in window title.
+  if (frametime >= 1000) {
+    char window_title[128];
+    snprintf(window_title, 128,
+            "Box2D: %f fps, level %d/%d",
+            frame_count / (frametime / 1000.f), -1, num_levels);
+    glutSetWindowTitle(window_title);
+    last_time = time;
+    frame_count = 0;
+  }
 }
 
 /*
